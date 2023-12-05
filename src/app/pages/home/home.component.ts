ea3eac860c0d2fa59559965b8eafca3d565af181
Edward <edwardapersil7@gmail.com>
2023-12-05 20:05:24 +0000

    homepage ui design

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,13 +3,15 @@ import { NavigationComponent } from "../../components/navigation/navigation.comp
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeroComponent } from "../../components/hero/hero.component";
+import { CardsComponent } from "../../components/cards/cards.component";
+import { FeaturesComponent } from "../../components/features/features.component";
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.css',
-    imports: [NavigationComponent, CommonModule, RouterOutlet, HeroComponent]
+    imports: [NavigationComponent, CommonModule, RouterOutlet, HeroComponent, CardsComponent, FeaturesComponent]
 })
 export class HomeComponent {
 
